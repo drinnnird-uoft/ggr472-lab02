c1b96c650aa07acbd76fc8916f5cb581f727e05a
drinnnird-uoft <drin.brown@alum.utoronto.ca>
2025-02-06 18:50:53 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHJpbm5pcmQiLCJhIjoiY201b2RyYXRhMGt1YTJvcHQ4ZjU4dDYycSJ9.jHNRKSu149-F5s157m1GwA'; // Add default public map token from your Mapbox account
+const map = new mapboxgl.Map({
+    container: 'my-map', // map container ID
+    style: 'mapbox://styles/drinnird/cm6twaxlw01bc01ryeat9ddox', // style URL
+    center: [-79.39, 43.662], // starting position [lng, lat]
+    zoom: 4
+});
